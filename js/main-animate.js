db45f94ac49526c7e9e1570025c346f4501c50a1
Konstantin <163849606+it2konst@users.noreply.github.com>
2024-12-02 13:09:49 +0700

    animate page added

---
+++ js/main-animate.js
@@ -0,0 +1,32 @@
+ScrollReveal({
+    reset: true,
+    distance: "80px",
+    duration: 1000,
+    delay: 100,
+});
+
+ScrollReveal().reveal(".disclaimer", { delay: 200, origin: "top" });
+
+ScrollReveal().reveal(".header__logo", { delay: 200, origin: "left" });
+ScrollReveal().reveal(".nav-menu__list li", { delay: 200, origin: "bottom", interval: 100 });
+ScrollReveal().reveal(".header__search", { delay: 300, origin: "right" });
+
+ScrollReveal().reveal(".hero__content h1", { delay: 300, origin: "left" });
+ScrollReveal().reveal(".hero__content p, .hero__content a", { delay: 300, origin: "right", interval: 200 });
+
+ScrollReveal().reveal(".features", { delay: 300, origin: "bottom" });
+
+ScrollReveal().reveal(".title-h2, .title-description", { delay: 100, origin: "left", interval: 200 });
+ScrollReveal().reveal(".btn", { delay: 200, origin: "bottom" });
+
+ScrollReveal().reveal(".features__wrapper h2, .features__wrapper span", { delay: 300, origin: "right", interval: 200 });
+
+ScrollReveal().reveal(".grid-container img", { delay: 200, origin: "bottom", interval: 100 });
+
+ScrollReveal().reveal(".import__stat p", { delay: 200, origin: "right", interval: 100 });
+ScrollReveal().reveal(".import img", { delay: 300, origin: "right" });
+
+ScrollReveal().reveal(".clients__list img", { delay: 200, origin: "left", interval: 100 });
+ScrollReveal().reveal(".clients .myslider-prev img, .clients .myslider-next img", { delay: 200, origin: "top", interval: 100 });
+
+ScrollReveal().reveal(".news__item img, .news__item p", { delay: 200, origin: "bottom", interval: 100 });
