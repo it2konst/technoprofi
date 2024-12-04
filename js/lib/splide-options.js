d48c2dd596fc9c03b9de7dacbe172ac95dff7946
Konstantin <163849606+it2konst@users.noreply.github.com>
2024-12-04 08:35:53 +0700

    Page about added

---
+++ js/lib/splide-options.js
@@ -0,0 +1,13 @@
+const mainSlider = new Splide("#main-slider", {
+    type: "loop",
+
+    perPage: 1,
+    perMove: 1,
+
+    pagination: false,
+    arrows: false,
+
+    lazyLoad: true,
+    autoplay: true,
+    interval: 3000,
+}).mount();
